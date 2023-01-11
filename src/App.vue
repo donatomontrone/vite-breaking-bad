5d284196d729eb12b6e5e3e308d45bf37cb07685
donatomontrone <donato.montrone@outlook.com>
2023-01-11 15:34:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+<script>
+import store from './store.js';
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+<style lang="scss">
+@use './styles/partials/variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
