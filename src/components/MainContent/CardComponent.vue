c17e2cec706017d6480eaaaf4c2d6e552f7e346f
donatomontrone <donato.montrone@outlook.com>
2023-01-12 17:08:29 +0100

    add cards image hover

--- src/components/MainContent/CardComponent.vue
+++ src/components/MainContent/CardComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-6 col-md-3 col-lg-2 d-flex">
         <div class="card text-bg-dark mb-3">
-            <img :src="cardInfo.card_images[0].image_url" class="card-img-top" :alt="cardInfo.name">
+            <img :src="backImage" @mouseover="backImage = frontImage" class="card-img-top" :alt="cardInfo.name">
             <div class="card-body">
                 <h6 class="card-title text-center text-danger">{{ cardInfo.name }}</h6>
                 <p class="card-text text-center fw-lighter">{{ cardInfo.archetype }}</p>
@@ -11,9 +11,21 @@
 </template>
 <script>
 export default {
+    name: 'CardComponent',
     props: [
         "cardInfo"
-    ]
+    ],
+    data() {
+        return {
+            frontImage: this.cardInfo.card_images[0].image_url,
+            backImage: this.getImagePath('back.png')
+        }
+    },
+    methods: {
+        getImagePath: function (img) {
+            return new URL((`../../assets/img/${img}`), import.meta.url).href;
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
