819b270682ef0fa9d18d650f4fba40e0e8a87186
donatomontrone <donato.montrone@outlook.com>
2023-01-13 09:37:26 +0100

    remove mouseover on cards

--- src/components/MainContent/CardComponent.vue
+++ src/components/MainContent/CardComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-6 col-md-3 col-lg-2 d-flex">
         <div class="card text-bg-dark mb-3">
-            <img :src="backImage" @mouseover="backImage = frontImage" class="card-img-top" :alt="cardInfo.name">
+            <img :src="cardInfo.card_images[0].image_url" class="card-img-top" :alt="cardInfo.name">
             <div class="card-body">
                 <h6 class="card-title text-center text-danger">{{ cardInfo.name }}</h6>
                 <p class="card-text text-center fw-lighter">{{ cardInfo.archetype }}</p>
@@ -17,8 +17,6 @@ export default {
     ],
     data() {
         return {
-            frontImage: this.cardInfo.card_images[0].image_url,
-            backImage: this.getImagePath('back.png')
         }
     },
     methods: {
